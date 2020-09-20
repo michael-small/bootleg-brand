f785459879af3b7c536f6384e270afbc256c4069
michael-small <small203@morris.umn.edu>
2020-09-20 11:18:20 -0500

    Move homepage content to comp, create about comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
+import { AboutUsComponent } from "../app/components/about-us/about-us.component";
+import { HomeComponent } from "./components/home/home.component";
 
-const routes: Routes = [{ path: "home", component: AppComponent }];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "aboutUs", component: AboutUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,25 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { AboutUsComponent } from "./components/about-us/about-us.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 // Material modules
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule, MatCardModule } from "@angular/material";
+import { HomeComponent } from "./components/home/home.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AboutUsComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
